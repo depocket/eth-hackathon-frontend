d3ef22ceaaccf5883f9a9378b28a0abe2ec6e987
Himitsuko <himitsuko@github.com>
2022-11-24 20:48:48 +0700

    chore: theme

--- components/tokenflow/index.js
+++ components/tokenflow/index.js
@@ -17,55 +17,8 @@ export default function TokenFlow(props){
     setIsSSR(false);
   }, []);
   const [response, setResponse] = useState({
-    "nodes": [
-      {
-          "id": "0x33a7",
-          "label": "0x7c5fbb2de21b426ec6523fbb5d1bfbd818023a57",
-          "title": "address"
-      },
-      {
-          "id": "0x3496",
-          "label": "0xef7368f755f5f551a602971b1f792b2737bc6e16",
-          "title": "address"
-      },
-      {
-          "id": "0x3380",
-          "label": "0x9fb34d03374786a14e776d246f62eabdd9caaefe",
-          "title": "address"
-      },
-      {
-          "id": "0x337f",
-          "label": "0x89e73303049ee32919903c09e8de5629b84f59eb",
-          "title": "address"
-      }
-  ],
-  "edges": [
-      {
-          "from": "0x337f",
-          "to": "0x3380",
-          "label": "BUSD -> 0.01"
-      },
-      {
-          "from": "0x337f",
-          "to": "0x33a7",
-          "label": "BUSD -> 0.01"
-      },
-      {
-          "from": "0x3496",
-          "to": "0x33a7",
-          "label": "BUSD -> 338.410895156368947387"
-      },
-      {
-          "from": "0x3380",
-          "to": "0x337f",
-          "label": "BUSD -> 0.001"
-      },
-      {
-          "from": "0x3380",
-          "to": "0x337f",
-          "label": "BUSD -> 0.1"
-      }
-  ]
+    "nodes": [],
+    "edges": []
   });
   const options = {
     autoResize: true,
@@ -116,6 +69,8 @@ export default function TokenFlow(props){
           data.nodes = dt.nodes.map(function(node){
             var newNode = node
             newNode.label = format.getShortestAddress(node.label)
+            newNode.title = format.getShortestAddress(node.label)
+            newNode.value = node.label
             newNode.shape = 'circle'
             return newNode
           })
