1fb1169610d3cd559d0a15d951de2b148e2cff3f
Thang <pxthang97@gmail.com>
2022-11-25 15:41:51 +0700

    feat: improve token ui

--- components/tokenflow/index.js
+++ components/tokenflow/index.js
@@ -11,7 +11,7 @@ export default function TokenFlow(props){
   var [tokenAddress, setTokenAddress] = useState("")
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [depth, setDepth] = useState(10);
+  const [depth, setDepth] = useState(5);
   const [isSSR, setIsSSR] = useState(true);
   useEffect(() => {
     setIsSSR(false);
@@ -21,57 +21,67 @@ export default function TokenFlow(props){
     "edges": []
   });
   const options = {
+    nodes: {
+      shape: "dot",
+      scaling: {
+        min: 10,
+        max: 30,
+      },
+      font: {
+        size: 12,
+        face: "Tahoma",
+      },
+    },
     autoResize: true,
     layout: {
       hierarchical: false
     },
     edges: {
-      color: "#000000",
+      color: { inherit: true },
+      width: 0.15,
       smooth: {
-        enabled: true,
-        type: "discrete",
-        roundness: 0.5
-      }
+        type: "continuous",
+      },
     },
     height: "600px",
     width: "1200px",
-    physics: {
-      stabilization: true,
-    },
-    interaction: { 
+    physics: {barnesHut: {gravitationalConstant: -5000, springConstant: 0.001, springLength: 100}},
+    interaction: {
       dragView: true,
       hover: true,
       hoverConnectedEdges: true,
       selectable: true,
       selectConnectedEdges: true
-    }
+    },
   };
-    const handleSubmitClick = function(event) {
-      console.log(walletAddress, tokenAddress, startDate.toISOString(), endDate.toISOString())
-      fetch('https://1vrwtsghk9.execute-api.ap-southeast-1.amazonaws.com/production/v1/address/full-flow',
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        },
-        method: "POST",
-        body: JSON.stringify({
-          "depth": depth,
-          "address": walletAddress,
-          "token": tokenAddress,
-          "from": startDate.toISOString(),
-          "to": endDate.toISOString()
-        })
-      }).then(function(res){
-        (async function(){
-          var dt = await res.json()
-          var data = {}
+  const handleSubmitClick = function(event) {
+    console.log(walletAddress, tokenAddress, startDate.toISOString(), endDate.toISOString())
+    fetch('https://1vrwtsghk9.execute-api.ap-southeast-1.amazonaws.com/production/v1/address/full-flow',
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          },
+          method: "POST",
+          body: JSON.stringify({
+            "depth": depth,
+            "address": walletAddress,
+            "token": tokenAddress,
+            "from": startDate.toISOString(),
+            "to": endDate.toISOString()
+          })
+        }).then(function(res){
+      (async function(){
+        var dt = await res.json()
+        var data = {}
+        if (dt.nodes) {
           data.nodes = dt.nodes.map(function(node){
             var newNode = node
             newNode.label = format.getShortestAddress(node.label)
             newNode.title = format.getShortestAddress(node.label)
             newNode.value = node.label
             newNode.shape = 'circle'
+            newNode.color = node.color
             return newNode
           })
           data.edges = dt.edges.map(function(edge){
@@ -79,15 +89,17 @@ export default function TokenFlow(props){
             return newEdge
           })
           setResponse(dt)
-        })()
-      }).catch(function(e){
-        console.log(e)
-      })
-      event.preventDefault();
-    }
-    return (
-        <div>
-          <div className={styles.graph_menu}>
+        }
+
+      })()
+    }).catch(function(e){
+      console.log(e)
+    })
+    event.preventDefault();
+  }
+  return (
+      <div>
+        <div className={styles.graph_menu}>
           <form>
             <input value={walletAddress} onChange={function(event){setWalletAddress(event.target.value)}} className={componentStyles.form_input} type="text" name="wallet_address" placeholder="Enter wallet address"/>
             <input value={tokenAddress} onChange={function(event){setTokenAddress(event.target.value)}} className={componentStyles.form_input} type="text" name="token_address" placeholder="Enter token address"/>
@@ -101,15 +113,15 @@ export default function TokenFlow(props){
             </label>
             <button className={componentStyles.form_button} onClick={handleSubmitClick}>Scan</button>
           </form>
-          </div>
-          <div className={styles.graph}>
-            {!isSSR && console.log(response)}
-            {!isSSR && <Graph
+        </div>
+        <div className={styles.graph}>
+          {!isSSR && console.log(response)}
+          {!isSSR && <Graph
               graph={response}
               options={options}
-            />
-            }
-          </div>
+          />
+          }
         </div>
-    )
+      </div>
+  )
 }
